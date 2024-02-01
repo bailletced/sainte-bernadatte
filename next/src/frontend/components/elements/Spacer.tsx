821cbf41179df1221cfe4f260b7d917983d0e20c
antPinot <pinot.anthony@gmail.com>
2024-02-01 14:45:28 +0100

    Changement background Spacer

--- next/src/frontend/components/elements/Spacer.tsx
+++ next/src/frontend/components/elements/Spacer.tsx
@@ -4,6 +4,10 @@ import { TSizeProps } from "@/src/frontend/types/style/sizes";
 
 export const Spacer: TFC<TSizeProps> = ({ size }) => (
   <div
-    className={clsx(size?.toString()?.includes("w") && "float-left", size)}
+    className={clsx(
+      size?.toString()?.includes("w") && "float-left",
+      size,
+      "flex-grow bg-[#f7f9fc]"
+    )}
   />
 );
